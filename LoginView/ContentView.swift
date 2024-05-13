fe84daa5f6f5dae2c62beff6518bba3bf633d72b
realzare <realhosseinzare@gmail.com>
2024-05-13 16:58:11 +0330

    Initial Commit

---
+++ LoginView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginView
+//
+//  Created by Hossein Zare on 5/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginView/LoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewApp.swift
+//  LoginView
+//
+//  Created by Hossein Zare on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
